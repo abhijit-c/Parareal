2c9238428990318744a9f1ce486facc8d8ebe3e0
abhijit-c <abhijit9331@gmail.com>
2019-05-03 19:30:25 -0400

    Working on MATLAB version of parareal, for some reason not converging.

--- src/testing/parareal.m
+++ src/testing/parareal.m
@@ -1,22 +1,33 @@
+method = @(ode, y0, t_range, dt) fweuler(ode, y0, t_range, dt);
+y0 = 0.1;
 ode = @(t,y) y;
-y0 = 1;
-T = 10;
-dt = 1;
+T = 5;
+dt = .1;
 
-figure();
-hold on;
+num_para_its = 5;
+num_temporal_its = 50;
+U = zeros(num_temporal_its+1, num_para_its+1);
+U(1,:) = y0;
+[t, U(:,1)] = method(ode, y0, [0, T], dt);
 
-[t_course, y_course] = fweuler(ode, y0, [0, T], dt);
-plot(t_course, y_course, 'DisplayName', 'Course');
-
-for k=1:numel(t_course)-1
-  [~, y_fine] = fweuler(ode, y_course(k), t_course(k:k+1)', 1/1000);
-  y_course(k+1) = y_fine(end);
+for k=1:num_para_its
+  for n=1:num_temporal_its % Correction iteration (Parallel capable)
+    [~, G] = method(ode, U(n,k+1), t(n:n+1)', dt);
+    [~, F] = method(ode, U(n,k), t(n:n+1)', dt^2);
+    U(n+1,k+1) = G(end) + F(end) - U(n,k);
+  end
 end
 
-plot(t_course, y_course, '--', 'DisplayName', '1-Parareal');
-plot(t_course, exp(t_course), 'DisplayName', 'True');
+[tf, Fine] = method(ode, y0, [0, T], dt^2);
+figure();
 
+hold on;
+for k=0:num_para_its
+  plot(t, U(:,k+1), 'DisplayName', sprintf('%d-parareal',k));
+end
+plot(tf, Fine, '--', 'DisplayName', 'Full Fine Solution');
+plot(t, y0*exp(t), 'k-s', 'DisplayName', 'True Solution');
 hold off;
-title('One Parareal comparison'); ylabel('y(t)'); xlabel('t in [0,1]')
-legend();
+
+title('Parareal on y'' = y, y(0) = 1'); ylabel('y(t)'); xlabel('t in [0,1]');
+legend('Location', 'northeast');
