5cd0dabc6d9afc12069f8a4d28038d5734b700de
abhijit-c <abhijit9331@gmail.com>
2019-05-11 23:52:38 -0400

    Testing file for ode_integrators

---
+++ src/C++/test.cpp
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include "ode_integrators/integrators.h"
+
+int rhs(double t, double *yf, double *dydt)
+{
+  dydt[0] = yf[0];
+  return 0;
+}
+
+int main()
+{
+  ode_system ode;
+  ode.dimension = 1; ode.t_init = 0; ode.t_final = 4;
+  ode.y0 = (double *)malloc(sizeof(double)); 
+  ode.y0[0] = 1;
+  ode.f = std::function<int(double, double*, double*)>(&rhs);
+
+  double *yf = (double *)malloc(sizeof(double)); yf[0] = 0;
+
+  forward_euler(ode, .001, yf);
+  printf("FW: Computed: %f, error = %e\n", yf[0], exp(ode.t_final)-yf[0]);
+
+  rk4(ode, .001, yf);
+  printf("RK4: Computed: %f, error = %e\n", yf[0], exp(ode.t_final)-yf[0]);
+
+  free(yf);
+  free(ode.y0);
+}
