7d5b64154fec90e554fec6376494e2be99d7020b
abhijit-c <abhijit9331@gmail.com>
2019-05-15 18:59:21 -0400

    Work on parareal

--- src/C++/ode_integrators/parareal.cpp
+++ src/C++/ode_integrators/parareal.cpp
@@ -5,6 +5,8 @@ int parareal(ode_system sys,
              time_stepper course, time_stepper fine, 
              Eigen::VectorXd &yf)
 {
-  //int P = omp_get_max_threads();
+  int D = sys.dimension;
+  Eigen::VectorXd y_c(D); 
+  course.sstep_integrate(sys, y_c);
   return 0;
 }
