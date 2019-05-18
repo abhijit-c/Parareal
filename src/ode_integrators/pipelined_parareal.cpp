d03cb2a6cf52189fcb6b2fde7cdf567512e8143f
abhijit-c <abhijit9331@gmail.com>
2019-05-18 02:22:22 -0400

    Finished writing pipelined parareal, great success!!!!

--- src/ode_integrators/pipelined_parareal.cpp
+++ src/ode_integrators/pipelined_parareal.cpp
@@ -11,7 +11,7 @@ int pipelined_parareal(ode_system &sys, time_stepper course, time_stepper fine,
   for (int i = 0; i < csteps - 1; i++) { omp_init_lock(&(lock[i])); }
 
   // Initialize course/fine temporary structures
-  Eigen::MatrixXd ycourse(csteps, D), yfine(csteps, D);
+  Eigen::MatrixXd ycourse(csteps, D), yfine(csteps, D), delta_y(csteps, D);
   ycourse.row(0) = sys.y0; yfine.row(0) = sys.y0; yf.row(0) = sys.y0;
   Eigen::VectorXd tt(csteps);
   for (int k = 0; k < csteps; k++) { tt(k) = sys.t_init + k*course.dt; }
@@ -34,24 +34,44 @@ int pipelined_parareal(ode_system &sys, time_stepper course, time_stepper fine,
       temp_sys.t_init = tt(p); temp_sys.t_final = tt(p+1);
       temp_sys.y0 = y_temp;
       course.integrate(temp_sys, y_temp);
-      ycourse.row(p) = y_temp;
+      ycourse.row(p+1) = y_temp;
     } // END initial initial solve NOWAIT
 
     for (int k = 1; k < para_its; k++)
     { // BEGIN parareal iterations
-      #pragma omp for ordered // ORDERED IMPORTANT
+      #pragma omp for nowait ordered // ORDERED IMPORTANT
       for (int p = 0; p < N; p++)
       { //BEGIN processor p computation
         ode_system temp_sys = sys;
         Eigen::VectorXd y_temp(D);
 
-        temp_sys.t_init = sys.t_init+p*course.dt;
-        temp_sys.t_final = sys.t_init+p*course.dt;
-        temp_sys.y0 = 
+        // Compute fine solution and corrector term for pth node.
+        temp_sys.t_init = tt(p); temp_sys.t_final = tt(p+1);
+        temp_sys.y0 = yf.row(p);
         omp_set_lock(&(lock[p]));
+        fine.integrate(temp_sys, y_temp);
+        delta_y.row(p+1) = y_temp - ycourse.row(p+1);
+        omp_unset_lock(&(lock[p]));
 
-      }
-    }
-  }
+        #pragma omp ordered
+        { // BEGIN ordered region
+          temp_sys.t_init = tt(p); temp_sys.t_final = tt(p+1);
+          temp_sys.y0 = yf.row(p);
+          course.integrate(temp_sys, y_temp);
+          ycourse.row(p+1) = y_temp;
+          if (p != N-1)
+          { // Corrector step.
+            omp_set_lock(&(lock[p+1]));
+            yf.row(p+1) = ycourse.row(p+1) + delta_y.row(p+1);
+            omp_unset_lock(&(lock[p+1]));
+          }
+        } // END ordered region
+      } // END processor p computation
+    } // END parareal iterations
+  } // END pipelined parareal 
+
+  // Clean up space.
+  for (int i = 0; i < csteps - 1; i++) { omp_destroy_lock(&(lock[i])); }
+
   return 0;
 }
