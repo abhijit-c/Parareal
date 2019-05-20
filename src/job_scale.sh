8d9b64f50a0aa957c3d8bf1fe42bb36cabbb5cc0
abhijit-c <abhijit9331@gmail.com>
2019-05-20 18:40:47 -0400

    Updated job process, and changed name of strong scaling job

--- src/job_scale.sh
+++ src/job_scale.sh
@@ -2,14 +2,15 @@
 #
 #SBATCH --verbose
 #SBATCH --job-name=ac6361_parareal
-#SBATCH --output=parareal_%j.out
+#SBATCH --output=parareal_%j.txt
 #SBATCH --err=parareal_%j.err
-#SBATCH --time=00:20:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=28
+#SBATCH --cpus-per-task=40
 #SBATCH --mem=4GB
 #SBATCH --mail-type=END
 #SBATCH --mail-user=ac6361@nyu.edu
  
-srun ./linear1d_test.out
+srun ./strongscale_test.out
+srun ./weakscale_test.out
