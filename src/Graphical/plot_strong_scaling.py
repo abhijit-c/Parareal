441ad9b313998f0360d37cbf1f677c4d176a2c9e
abhijit-c <abhijit9331@gmail.com>
2019-05-20 15:50:26 -0400

    Added plotting txt files, cleaned up codes

---
+++ src/Graphical/plot_strong_scaling.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+TFINE = .4555
+
+plt.figure()
+
+P = np.transpose(np.arange(1,29))
+data = np.loadtxt(f'Data/scalability_k4.txt')
+plt.plot(P, data, 's-', label='4-Parareal')
+speedstr = f'Speedup: {TFINE/data[-1]:.2f}'
+data = np.loadtxt(f'Data/scalability_k6.txt')
+plt.plot(P, data, 's-', label='6-Parareal')
+plt.plot(P, TFINE*np.ones(P.shape), label='Serial Computation')
+
+plt.title('Strong Scaling Study for Parareal')
+plt.xlabel('Number of Processors')
+plt.ylabel('Time taken in seconds')
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+plt.text(19.0,1.0, speedstr, fontsize=14, bbox=props)
+plt.legend(loc='upper right', bbox_to_anchor=(1, 1),
+          ncol=1, fancybox=True, shadow=True)
+
+
+
+plt.tight_layout()
+plt.show()
