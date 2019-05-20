95edb5bbaf4d02dcaea3ff899c7cfc671d8ff97a
abhijit-c <abhijit9331@gmail.com>
2019-05-20 14:07:19 -0400

    Weak scaling tests completed

--- src/linear1d_test.cpp
+++ src/linear1d_test.cpp
@@ -29,7 +29,7 @@ void test_method(ode_system &ode, time_stepper course, time_stepper fine)
   for (int j = 0; j < 10; j++)
   {
     tt = omp_get_wtime();
-    parareal(ode, course, fine, 6, yf);
+    parareal(ode, course, fine, 14, yf);
     avg += omp_get_wtime() - tt;
   }
   //std::cout << yf << std::endl;
@@ -78,51 +78,31 @@ void write_fine(ode_system &ode, time_stepper course, time_stepper fine)
 
 int main(int argc, char **argv)
 {
-
-  ode_system ode;
-  ode.dimension = 1; ode.t_init = 0; ode.t_final = 4;
-  ode.y0 = Evec(1); ode.y0(0) = 1;
-  ode.f = std::function<int(double, Evec&, Evec&)>(&rhs);
-
-  time_stepper course; time_stepper fine;
-  // Forward Euler 
-  course.dt = 4.0/( (double) omp_get_max_threads() );
-  course.F = std::function<int(ode_system&, double, Evec &)>(&forward_euler);
-  course.F_allt = std::function<int(ode_system&, double, Emat &)>(&forward_euler_allt);
-  fine.dt = .0000001;
-  fine.F = std::function<int(ode_system&, double, Evec &)>(&forward_euler);
-  fine.F_allt = std::function<int(ode_system&, double, Emat &)>(&forward_euler_allt);
-
   int P = omp_get_max_threads();
-
-  int fsteps = ode.num_steps(fine.dt);
-  Emat yf_fine(fsteps, ode.dimension);
-  double tt = omp_get_wtime();
-  fine.integrate_allt(ode, yf_fine);
-  tt = omp_get_wtime() - tt;
-  //std::cout << yf_fine << std::endl;
-  printf("Fine computed in %fs\n", tt);
-  for (int k = 1; k <= P; k++)
+  for (int k = 2; k <= P; k++)
   {
     omp_set_num_threads(k);
-    printf("Using %d processors\n", omp_get_max_threads());
+    ode_system ode;
+    ode.dimension = 1; ode.t_init = 0; ode.t_final = k;
+    ode.y0 = Evec(1); ode.y0(0) = 1;
+    ode.f = std::function<int(double, Evec&, Evec&)>(&rhs);
+
+    time_stepper course; time_stepper fine;
+
+    // Forward Euler 
+    //omp_set_num_threads(k);
+    course.dt = 0.5;
+    course.F = std::function<int(ode_system&, double, Evec &)>(&forward_euler);
+    course.F_allt = std::function<int(ode_system&, double, Emat &)>(&forward_euler_allt);
+    fine.dt = .000001;
+    fine.F = std::function<int(ode_system&, double, Evec &)>(&forward_euler);
+    fine.F_allt = std::function<int(ode_system&, double, Emat &)>(&forward_euler_allt);
+
+    printf("Using %d processors to solve up to T = %f\n", k, ode.t_final);
     //write_parareal(ode, course, fine);
     //write_fine(ode, course, fine);
     test_method(ode, course, fine);
   }
-
-  
-  /*
-  // rk4 Testing 
-  course.dt = .5;
-  course.F = std::function<int(ode_system&, double, Evec &)>(&rk4);
-  course.F_allt = std::function<int(ode_system&, double, Emat &)>(&rk4_allt);
-  fine.dt = .0000001;
-  fine.F = std::function<int(ode_system&, double, Evec &)>(&rk4);
-  fine.F_allt = std::function<int(ode_system&, double, Emat &)>(&rk4_allt);
-  printf("RK4 tests:\n");
-  test_method(ode, course, fine);
-  */
 
   return 0;
 }
